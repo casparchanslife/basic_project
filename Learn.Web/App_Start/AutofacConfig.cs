2fdafa47ceb388bc8699883e90d4be83e6906b26
caspar <caspar_chan@mtelnet.com>
2018-04-08 20:49:26 +0800

    change autofac config

--- Learn.Web/App_Start/AutofacConfig.cs
+++ Learn.Web/App_Start/AutofacConfig.cs
@@ -11,6 +11,7 @@ using Learn.Lib.Infrastructure.Repositories;
 using Learn.Web.Service.Services;
 using Learn.Data;
 using Learn.DataModel.Models;
+using Learn.Web;
 
 namespace Learn
 {
@@ -27,16 +28,20 @@ namespace Learn
             builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerHttpRequest();
             builder.RegisterType<DatabaseFactory>().As<IDatabaseFactory>().InstancePerHttpRequest();
             builder.RegisterAssemblyTypes(typeof(NoteRepository).Assembly)
-            .Where(t => t.Name.EndsWith("Repository"))
-            .AsImplementedInterfaces().InstancePerHttpRequest();
+                   .Where(t => t.Name.EndsWith("Repository"))
+                   .AsImplementedInterfaces()
+                   .InstancePerHttpRequest();
             builder.RegisterAssemblyTypes(typeof(NoteService).Assembly)
-           .Where(t => t.Name.EndsWith("Service"))
-           .AsImplementedInterfaces().InstancePerHttpRequest();
+                   .Where(t => t.Name.EndsWith("Service"))
+                   .AsImplementedInterfaces()
+                   .InstancePerHttpRequest();
 
+            builder.RegisterControllers(typeof(MvcApplication).Assembly)
+             .InstancePerHttpRequest();
             //builder.Register(c => new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext())))
             //    .As<UserManager<ApplicationUser>>().InstancePerHttpRequest();
 
-            builder.RegisterFilterProvider();
+            builder.RegisterModule<AutofacWebTypesModule>();
             IContainer container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
