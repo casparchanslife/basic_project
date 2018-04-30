9d2dc9027a65832ddc6505f4ca1f2c47bf991cc3
caspar <caspar_chan@mtelnet.com>
2018-05-01 01:52:18 +0800

    c

---
+++ Learn.ViewModel/AccessRightModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.ViewModel
+{
+    public class AccessRightModel
+    {
+        public string FunctionId { get; set; }
+        public bool Insert { get; set; }
+        public bool Delete { get; set; }
+        public bool Update { get; set; }
+        public bool Copy { get; set; }
+        public bool Read { get; set; }
+    }
+}
